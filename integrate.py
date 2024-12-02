ab017d8744646ebf97935225fcf17511aad9f554
Anwar Gatto <37763509+RyzenGatto@users.noreply.github.com>
2024-12-01 23:35:29 -0700

    Update integrate.py

--- integrate.py
+++ integrate.py
@@ -1,22 +1,27 @@
 import math
 from sympy import *
 
-
+pi = math.pi
 x = symbols('x')
 
-pi = math.pi
+# lower bounds
+a =
+#upper bounds
+b =
 
+#input function + example (for RMS, you must square your original function before integration. This function is squared with **2. Otherwise, remove **2
 function = (30*sin(2*pi*60*x) + 40*sin(2*pi*120*x))**2
 
 r = integrate(function)
 
-F1 = r.subs(x, 0)
-F2 = r.subs(x, math.pi)
+#Evaluates the antiderivative at 'a' and 'b'
+F1 = r.subs(x, a)
+F2 = r.subs(x, b)
 
 Fund = F2 - F1
 
-AVG = Fund/math.pi
+AVG = Fund/(b-a)
 
-FDM = math.sqrt(AVG)
+RMS = math.sqrt(AVG)
 
-print(F1, F2, Fund, FDM)
+print(F1, F2, Fund, RMS)
